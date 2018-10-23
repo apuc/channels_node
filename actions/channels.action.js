562039a467fc9eef5f6eea93e0584ed5af3ee72d
yura <yurafront@gmail.com>
2018-10-23 11:05:23 +0300

    add try/catch

--- actions/channels.action.js
+++ actions/channels.action.js
@@ -3,29 +3,44 @@ const { connectedUsers } = require('../app');
 module.exports.channelsAction = (socket, io) => {
 
   socket.on('joinChannels', function (channelsIds) {
+    try {
       const { username } = connectedUsers[socket.id];
 
       channelsIds.forEach(id => {
         socket.join(id);
         socket.emit('systemMessage', {message: `You joined to ${id}`});
       });
+    } catch (e) {
+      console.error(e)
+    }
+
   });
 
   socket.on('joinChannel', function (channelId) {
-    const { username } = connectedUsers[socket.id];
+    try {
+      const { username } = connectedUsers[socket.id];
 
-    socket.join(channelId);
-    socket.emit('systemMessage', {message: `You joined to ${channelId}`});
-    socket.broadcast.to(channelId).emit('systemMessage', {message: `${username} joined!`});
+      socket.join(channelId);
+      socket.emit('systemMessage', {message: `You joined to ${channelId}`});
+      socket.broadcast.to(channelId).emit('systemMessage', {message: `${username} joined!`});
+    } catch (e) {
+      console.error(e)
+    }
+
   });
 
   socket.on('leaveChannel', function (channelId) {
+    try {
       const { username } = connectedUsers[socket.id];
 
       socket.leave(channelId, () => {
-          socket.emit('systemMessage', {message: `You leave channel ${channelId}!`});
-          socket.broadcast.to(channelId).emit('systemMessage', {message: `${username} leave channel!`});
+        socket.emit('systemMessage', {message: `You leave channel ${channelId}!`});
+        socket.broadcast.to(channelId).emit('systemMessage', {message: `${username} leave channel!`});
       });
+    } catch (e) {
+      console.error(e)
+    }
+
   });
 
 };
