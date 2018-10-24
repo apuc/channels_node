32a983bde3d6a1bdebc151cc85941e4852a199cf
Volizik <kizilov.yura@gmail.com>
2018-10-24 21:55:05 +0300

    returning vue

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const router = express.Router();
 
 const port = process.env.PORT || 2368;
 const connectedUsers = [];
 
-app.get('/*', (req, res) => {
-  res.status(404).end();
+app.use(express.static('../vue_channels/dist'));
+
+app.set('views', '../vue_channels/dist');
+
+router.get('/*', (req, res, next) => {
+    res.sendFile('../vue_channels/dist/index.html');
 });
 
 http.listen(port, () => { console.log(`listening on port ${port}`); });
