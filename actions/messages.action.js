46271b00555773dfba29d996b28379c98012b960
Volizik <kizilov.yura@gmail.com>
2018-11-06 22:30:06 +0300

    mesasages

--- actions/messages.action.js
+++ actions/messages.action.js
@@ -1,5 +1,8 @@
 const { connectedUsers } = require('../app');
 const request = require('request');
+request.defaults({
+    headers: {'Content-Type': 'application/json'}
+});
 
 module.exports.messagesAction = (socket, io) => {
 
@@ -8,12 +11,22 @@ module.exports.messagesAction = (socket, io) => {
     });
 
     socket.on('userMessage', messageData => {
-        const { channel_id } = messageData;
-        request.post(`${process.env.API_URL}/message`, messageData.text)
-            .on('response', res => {
-                console.log('res', res.statusCode)
-            })
-            .on('error', err => console.log(err));
+        const { channel_id, text, from } = messageData;
+        const message = {
+            channel_id: channel_id.toString(),
+            text,
+            from: from.toString()
+        };
+        console.log(message);
+        request.post(`${process.env.API_URL}/message`, message, function (err, res, body) {
+            // console.log('err', err)
+            // console.log('res', res)
+            console.log('body', body)
+        })
+            // .on('response', res => {
+            //     console.log('res', res)
+            // })
+            // .on('error', err => console.log(err));
         io.to(channel_id).emit('userMessage', messageData);
     });
 
