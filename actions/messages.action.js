0e36436f9524292677efeaac08b4f42b081fcfbf
yura <yurafront@gmail.com>
2018-11-08 15:06:46 +0300

    send message

--- actions/messages.action.js
+++ actions/messages.action.js
@@ -1,11 +1,4 @@
 const request = require('request');
-const r = request.defaults({
-    headers: {
-      'Accept': '*/*',
-      'content-type': 'application/json',
-      'Cache-Control': 'no-cache'
-    }
-});
 
 module.exports.messagesAction = (socket, io) => {
 
@@ -15,13 +8,17 @@ module.exports.messagesAction = (socket, io) => {
 
     socket.on('userMessage', messageData => {
         const { channel_id, text, from } = messageData;
-        const message = JSON.stringify({ channel_id, from, text });
-        console.log(message);
-        r.post(`${process.env.API_URL}/message`, message, function (err, res, body) {
+
+        request.post({
+          url: `${process.env.API_URL}/message`,
+          form: { channel_id, from, text }
+        }, function (err, res, body) {
             // console.log('err', err)
             // console.log('res', res)
-            console.log('body', body)
+            console.log('body', body);
+            // io.to(channel_id).emit('messageStatus', 'ОК');
         });
+
         io.to(channel_id).emit('userMessage', messageData);
     });
 
