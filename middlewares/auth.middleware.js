7e67405ea467c7270eec6e1075f21a02b675a98d
Volizik <kizilov.yura@gmail.com>
2018-11-10 23:12:00 +0300

    token auth

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,9 +1,23 @@
+const request = require('request');
+
 module.exports.isAuthorized = (socket, next) => {
-  const token = socket.handshake.query.token;
-  // console.log(token);
-  // make sure the handshake data looks good as before
-  // if error do this:
-  // next(new Error('From socket: Not authorized'));
-  // else just call next
-  next();
+    const token = socket.handshake.query['token'];
+    request.get({
+        url: `${process.env.API_URL}/user/me`,
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        }
+    }, function (err, res) {
+        if (err) {
+            next(new Error(err));
+        } else if (res.statusCode === 200) {
+            next();
+        } else if (res.statusCode === 401) {
+            next(new Error(`Error from socket: Not authenticated! Status code: ${res.statusCode}`));
+        } else {
+            next(new Error(`Error from socket: Status code ${res.statusCode}`))
+        }
+    });
 };
