79a7e4718a8e125144072f3aaa74d01fc25aea56
yura <yurafront@gmail.com>
2018-11-27 16:21:43 +0300

    добавил https соединение

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const privateKey  = fs.readFileSync('privkey.pem', 'utf8');
+const certificate = fs.readFileSync('fullchain.pem', 'utf8');
+const options = {key: privateKey, cert: certificate};
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const https = require('https').Server(options, app);
+const io = require('socket.io')(https);
 const env = require('dotenv').config();
 const helmet = require('helmet');
 const path = require('path');
@@ -14,5 +19,6 @@ app.use(express.static(path.join(__dirname, '../vue_channels/dist')));
 app.use('*', router);
 
 http.listen(port, () => { console.log(`listening on port ${port}`); });
+https.listen(2369, () => { console.log(`listening on port 2369`); });
 
 module.exports.io = io;
